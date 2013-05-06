b49286f53aace1490fb144fd48181520c078b47e
Joseph Lee <joseph.lee22590@gmail.com>
2013-05-06 16:40:35 -0700

    Added translatable strings for the help dictinoary.

--- addon/globalPlugins/controlUsageAssistant/ctrltypelist.py
+++ addon/globalPlugins/controlUsageAssistant/ctrltypelist.py
@@ -11,23 +11,25 @@ helpMessages = {
 	# Translators: Help message for a checkbox.
 	5:_("Press space to check or uncheck the checkbox"),
 	# Translators: Help message for a combo box.
-	6:"Use the arrow keys to choose an option",
+	6:_("Use the arrow keys to choose an option"),
 	# Translators: Help message for a list box.
-	7:"Use the arrow keys to read this box",
+	7:_("Use the arrow keys to read this box"),
 	# Translators: Help message for an edit field.
-	8:"Type text here",
+	8:_("Type text here"),
 	# Translators: Help message for a read-only control.
 	-8:_("Use the text navigation commands to read text"),
 	# Translators: Help message for a button.
-	9:"Press SPACE or ENTER to activate this button",
+	9:_("Press SPACE or ENTER to activate this button"),
 	# Translators: Help message for menu items.
-	11:"Use the arrow keys to move between the menu items",
+	11:_("Use the arrow keys to move between the menu items"),
 	# Translators: Help message for a combo box.
-	13:"Use the arrow keys to move among choices in the combo box until the desired option is found",
+	13:_("Use the arrow keys to move among choices in the combo box until the desired option is found"),
 	# Translators: Help message for a list view.
-	15:"Use the arrow keys to move to the next or previous item in this list",
+	15:_("Use the arrow keys to move to the next or previous item in this list"),
+	# Translators: Help message for working with tree view items.
+	21:_("Use the up and down arrow keys to select the items. Use left arrow to collapse and right arrow to expand"),
 	# Translators: Help message for navigating tables.
-	28:"Press control, alt and arrow keys together to move between rows and columns",
+	28:_("Press control, alt and arrow keys together to move between rows and columns"),
 	# Translators: Help message for reading documents (mostly encountered in Internet Explorer windows).
-	52:"Use the arrow keys or object navigation commands to move through the document"
+	52:_("Use the arrow keys or object navigation commands to move through the document")
 	}
