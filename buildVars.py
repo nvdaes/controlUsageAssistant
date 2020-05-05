1699e9757ae7f6254c51ebc9af3b9a23857a6b01
Joseph Lee <joseph.lee22590@gmail.com>
2020-05-05 14:03:52 -0700

    BuildVars (Flake8 E203): no spaces before colon (add-on info dictionary)

--- buildVars.py
+++ buildVars.py
@@ -2,33 +2,33 @@
 # Change this file instead of sconstruct or manifest files, whenever possible.
 
 # Full getext (please don't change)
-_ = lambda x : x
+_ = lambda x: x
 
 # Add-on information variables
 addon_info = {
 	# add-on Name
-	"addon_name" : "controlUsageAssistant",
+	"addon_name": "controlUsageAssistant",
 	# Add-on description
 	# TRANSLATORS: Summary for this add-on to be shown on installation and add-on information.
-	"addon_summary" : _("Control Usage Assistant"),
+	"addon_summary": _("Control Usage Assistant"),
 	# Add-on description
 	# Translators: Long description to be shown for this add-on on installation and add-on information
-	"addon_description" : _("""Allows you to find out how to interact with the focused control, useful for new computer users new to Windows and to NvDA.
+	"addon_description": _("""Allows you to find out how to interact with the focused control, useful for new computer users new to Windows and to NvDA.
 	Press NvDA+H to get a short help message on using the focused control, such as moving through tables, checkboxes and so on."""),
 	# version
-	"addon_version" : "20.01",
+	"addon_version": "20.01",
 	# Author(s)
-	"addon_author" : "Joseph Lee <joseph.lee22590@gmail.com>",
+	"addon_author": "Joseph Lee <joseph.lee22590@gmail.com>",
 	# URL for the add-on documentation support
-	"addon_url" : 'http://addons.nvda-project.org',
+	"addon_url": "http://addons.nvda-project.org",
 	# File name for the add-on help file.
-	"addon_docFileName" : "readme.html",
+	"addon_docFileName": "readme.html",
 	# Minimum NVDA version supported
-	"addon_minimumNVDAVersion" : "2019.3.0",
+	"addon_minimumNVDAVersion": "2019.3.0",
 	# Last NVDA version supported/tested
-	"addon_lastTestedNVDAVersion" : "2020.2.0",
+	"addon_lastTestedNVDAVersion": "2020.2.0",
 	# Add-on update channel (default is stable)
-	"addon_updateChannel" : None,
+	"addon_updateChannel": None,
 }
 
 
