15e9f021ea61ac8d33862970db990500809760b5
Noelia Ruiz Martínez <nrm1977@gmail.com>
2026-02-08 15:19:14 +0100

    Update buildVars (#35)
    * Update
    
    * Format
    
    * Pre-commit auto-fix
    
    * Update version
    
    ---------
    
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

--- buildVars.py
+++ buildVars.py
@@ -1,100 +1,105 @@
-# -*- coding: UTF-8 -*-
-
 # Build customizations
 # Change this file instead of sconstruct or manifest files, whenever possible.
 
-# Use AddonInfo TypedDict for add-on information, as in the template
-from typing import TypedDict
-try:
-	from site_scons.site_tools.NVDATool.typings import AddonInfo
-except ImportError:
-	class AddonInfo(TypedDict):
-		addon_name: str
-		addon_summary: str
-		addon_description: str
-		addon_version: str
-		addon_changelog: str
-		addon_author: str
-		addon_url: str | None
-		addon_sourceURL: str | None
-		addon_docFileName: str
-		addon_minimumNVDAVersion: str | None
-		addon_lastTestedNVDAVersion: str | None
-		addon_updateChannel: str | None
-		addon_license: str | None
-		addon_licenseURL: str | None
+from site_scons.site_tools.NVDATool.typings import AddonInfo, BrailleTables, SymbolDictionaries
 
-def _(arg):
-	return arg
+# Since some strings in `addon_info` are translatable,
+# we need to include them in the .po files.
+# Gettext recognizes only strings given as parameters to the `_` function.
+# To avoid initializing translations in this module we simply import a "fake" `_` function
+# which returns whatever is given to it as an argument.
+from site_scons.site_tools.NVDATool.utils import _
 
-addon_info: AddonInfo = {
+
+# Add-on information variables
+addon_info = AddonInfo(
 	# add-on Name/identifier, internal for NVDA
-	"addon_name": "controlUsageAssistant",
+	addon_name="controlUsageAssistant",
 	# Add-on summary/title, usually the user visible name of the add-on
 	# Translators: Summary/title for this add-on
 	# to be shown on installation and add-on information found in add-on store
-	"addon_summary": _("Control Usage Assistant"),
+	addon_summary=_("Control Usage Assistant"),
 	# Add-on description
 	# Translators: Long description to be shown for this add-on on add-on information from add-on store
-	"addon_description": _("""Allows you to find out how to interact with the focused control, useful for new computer users new to Windows and to NVDA."""),
-	# Each key is the name of the dictionary,
+	addon_description=_("""Allows you to find out how to interact with the focused control, useful for new computer users new to Windows and to NVDA."""),
 	# version
-	"addon_version": "20250809.1.0",
+	addon_version="20250809.1.0",
 	# Brief changelog for this version
-	"addon_changelog": _(
-		# Translators: what's new content for the add-on version to be shown in the add-on store
-		"""* Requires NVDA 2026.1 or later.
-		* Added help message for toggle buttons.""",
- ),
-	# with keys inside recording the following attributes:
-	# displayName (name of the speech dictionary shown to users and translatable),
+	# Translators: what's new content for the add-on version to be shown in the add-on store
+	addon_changelog=_("""* Fixed changelog format.
+* Restored translations for add-on description."""),
 	# Author(s)
-	"addon_author": "Joseph Lee <joseph.lee22590@gmail.com>, Noelia Ruiz Martínez <nrm1977@gmail.com>",
+	addon_author="Joseph Lee <joseph.lee22590@gmail.com>, Noelia Ruiz Martínez <nrm1977@gmail.com>",
 	# URL for the add-on documentation support
-	"addon_url": "https://github.com/nvdaes/controlUsageAssistant",
+	addon_url="https://github.com/nvdaes/controlUsageAssistant",
 	# URL for the add-on repository where the source code can be found
-	"addon_sourceURL": None,
+	addon_sourceURL="https://github.com/nvdaes/controlUsageAssistant",
 	# Documentation file name
-	"addon_docFileName": "readme.html",
+	addon_docFileName="readme.html",
 	# Minimum NVDA version supported (e.g. "2019.3.0", minor version is optional)
-	"addon_minimumNVDAVersion": "2026.1",
+	addon_minimumNVDAVersion="2026.1",
 	# Last NVDA version supported/tested (e.g. "2024.4.0", ideally more recent than minimum version)
-	"addon_lastTestedNVDAVersion": "2026.1",
+	addon_lastTestedNVDAVersion="2026.1",
 	# Add-on update channel (default is None, denoting stable releases,
 	# and for development releases, use "dev".)
 	# Do not change unless you know what you are doing!
-	"addon_updateChannel": None,
+	addon_updateChannel=None,
 	# Add-on license such as GPL 2
-	"addon_license": None,
-	# URL for the license document the add-on is licensed under
-	"addon_licenseURL": None,
-}
-	# mandatory (True when always enabled, False when not.
-symbolDictionaries = {}
+	addon_license="GPL v2",
+	# URL for the license document the ad-on is licensed under
+	addon_licenseURL="https://www.gnu.org/licenses/gpl-2.0.html",
+)
 
 # Define the python files that are the sources of your add-on.
-# You can either list every file (using "/") as a path separator,
+# You can either list every file (using ""/") as a path separator,
 # or use glob expressions.
 # For example to include all files with a ".py" extension from the "globalPlugins" dir of your add-on
 # the list can be written as follows:
 # pythonSources = ["addon/globalPlugins/*.py"]
 # For more information on SCons Glob expressions please take a look at:
 # https://scons.org/doc/production/HTML/scons-user/apd.html
-pythonSources = ["addon/globalPlugins/controlUsageAssistant/*.py"]
+pythonSources: list[str] = [
+	"addon/*.py",
+	"addon/globalPlugins/controlUsageAssistant/*.py",
+]
 
 # Files that contain strings for translation. Usually your python sources
-i18nSources = pythonSources + ["buildVars.py"]
+i18nSources: list[str] = pythonSources + ["buildVars.py"]
 
 # Files that will be ignored when building the nvda-addon file
 # Paths are relative to the addon directory, not to the root directory of your addon sources.
-excludedFiles = []
+# You can either list every file (using ""/") as a path separator,
+# or use glob expressions.
+excludedFiles: list[str] = []
 
 # Base language for the NVDA add-on
 # If your add-on is written in a language other than english, modify this variable.
 # For example, set baseLanguage to "es" if your add-on is primarily written in spanish.
-baseLanguage = "en"
+# You must also edit .gitignore file to specify base language files to be ignored.
+baseLanguage: str = "en"
 
-markdownExtensions = []
+# Markdown extensions for add-on documentation
+# Most add-ons do not require additional Markdown extensions.
+# If you need to add support for markup such as tables, fill out the below list.
+# Extensions string must be of the form "markdown.extensions.extensionName"
+# e.g. "markdown.extensions.tables" to add tables.
+markdownExtensions: list[str] = []
 
-# List of braille tables used by the add-on (required by SConstruct)
-brailleTables = []
+# Custom braille translation tables
+# If your add-on includes custom braille tables (most will not), fill out this dictionary.
+# Each key is a dictionary named according to braille table file name,
+# with keys inside recording the following attributes:
+# displayName (name of the table shown to users and translatable),
+# contracted (contracted (True) or uncontracted (False) braille code),
+# output (shown in output table list),
+# input (shown in input table list).
+brailleTables: BrailleTables = {}
+
+# Custom speech symbol dictionaries
+# Symbol dictionary files reside in the locale folder, e.g. `locale\en`, and are named `symbols-<name>.dic`.
+# If your add-on includes custom speech symbol dictionaries (most will not), fill out this dictionary.
+# Each key is the name of the dictionary,
+# with keys inside recording the following attributes:
+# displayName (name of the speech dictionary shown to users and translatable),
+# mandatory (True when always enabled, False when not.
+symbolDictionaries: SymbolDictionaries = {}
